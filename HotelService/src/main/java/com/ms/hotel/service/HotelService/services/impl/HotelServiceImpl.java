cbb12eaea53b9abd8d01d8f37f4fabaf29308240
Suman Pal <sumanpal198@gmail.com>
2022-12-28 03:08:26 +0530

    Distributed Tracing : Adding Sleuth

--- HotelService/src/main/java/com/ms/hotel/service/HotelService/services/impl/HotelServiceImpl.java
+++ HotelService/src/main/java/com/ms/hotel/service/HotelService/services/impl/HotelServiceImpl.java
@@ -4,6 +4,7 @@ import com.ms.hotel.service.HotelService.entities.Hotel;
 import com.ms.hotel.service.HotelService.exceptions.ResourceNotFoundException;
 import com.ms.hotel.service.HotelService.repositories.HotelRepository;
 import com.ms.hotel.service.HotelService.services.HotelService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class HotelServiceImpl implements HotelService {
 
     @Autowired
@@ -18,20 +20,28 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public Hotel saveHotel(Hotel hotel) {
+        log.info("Entering into HotelServiceImpl : saveHotel");
         //generate unique userId
         String ranmdomHotelId = UUID.randomUUID().toString();
         hotel.setHotelId(ranmdomHotelId);
+        log.info("Exiting from HotelServiceImpl : saveHotel");
         return hotelRepository.save(hotel);
     }
 
     @Override
     public List<Hotel> getAllHotel() {
-        return hotelRepository.findAll();
+        log.info("Entering into HotelServiceImpl : getAllHotel");
+        List<Hotel> all = hotelRepository.findAll();
+        log.info("Exiting from HotelServiceImpl : getAllHotel");
+        return all;
     }
 
     @Override
     public Hotel getHotel(String hotelId) {
-        return hotelRepository.findById(hotelId).orElseThrow(
+        log.info("Entering into HotelServiceImpl : getHotel");
+        Hotel hotel = hotelRepository.findById(hotelId).orElseThrow(
                 () -> new ResourceNotFoundException("Hotel with given Id is not found on server !! : " + hotelId));
+        log.info("Exiting from HotelServiceImpl : getHotel");
+        return hotel;
     }
 }
